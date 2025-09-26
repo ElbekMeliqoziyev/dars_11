dded5e284468ea20a64a2965439fb8da84b558ff
Elbek <meliqoziyevelbek7@gmail.com>
2025-09-26 09:37:26 +0500

    task 3

--- problem_10.py
+++ problem_10.py
@@ -1,5 +1,15 @@
 """User klassi va undan meros olgan AdminUser
  va RegularUser klasslarini yarating. """
 
+class User:
+    def __init__(self, name):
+        self.name = name
 
+class AdminUser(User):
+    def __init__(self, name):
+        super().__init__(name)
+
+class RegularUser(User):
+    def __init__(self, name):
+        super().__init__(name)
     
